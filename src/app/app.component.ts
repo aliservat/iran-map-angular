316afc78bfb61cf860fdbb353796ee6990591f23
ali servat <ali.servat715@gmail.com>
2023-11-06 13:53:00 +0330

    iran map v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpData} from "./http.data";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'iran-map';
+  loading = true
+  loadedBox: any = ' '
+  city: any = 'qom'
+  cityId: any = 1
+  blog: any
+  cities: any
+
+  constructor(private data: HttpData) {
+  }
+
+
+  ngOnInit(): void {
+    this.blog = this.data.getBlog(this.cityId)
+    console.log(this.blog)
+
+    this.cities = this.data.getCities()
+    this.cities
+      .map((item: any) => {
+          let el = document.getElementById(item.fingilish)
+          // @ts-ignore
+          el.setAttribute("cityId", item.id);
+          // @ts-ignore
+          el.classList.remove('deActive')
+        }
+      )
+    setTimeout(() => {
+      this.loading = false
+    }, 1000)
+  }
+
+  isNum(value: any) {
+    return Number(value)
+  }
+
+  filterByCity(e: any, city: any) {
+    this.loadedBox = "loaded"
+    setTimeout(() => {
+      this.loading = true
+    }, 300) // تا انیمیشن خروج dom اجرا شود :)
+    this.city = city
+    let iranMap = document.querySelectorAll('.iranMap')
+    for (let i = 0; i < iranMap.length; ++i) {
+      iranMap[i].classList.remove('active');
+    }
+    e.target.parentElement.classList.toggle('active')
+    // @ts-ignore
+    this.cityId = document.getElementById(city).getAttribute('cityId')
+
+    setTimeout(() => {
+      this.blog = this.data.getBlog(this.cityId)
+      this.loading = false
+    }, 600) // درخواست به وب سرویس
+    setTimeout(()=>{
+      this.loadedBox = " "
+    },700)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([]),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
